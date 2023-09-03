6022e617cdf4a0e5c759c729c76e609eb0e252f1
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2023-09-03 11:04:43 -0500

    Add HttpClient Instrumentation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,14 +13,29 @@ using var tracerProvider = Sdk.CreateTracerProviderBuilder()
     .AddSource(myActivitySource.Name)
     .AddConsoleExporter() // Add OpenTelemetry.Exporter.Console package to use that
     .AddOtlpExporter() // 4317  Add OpenTelemetry.Exporter.OpenTelemetryProtocol package to use that
+    .AddHttpClientInstrumentation() // Add pre-release OpenTelemetry.Instrumentation.Http package to use that
     .Build();
 
-using (var activity = myActivitySource.StartActivity("Say Hello"))
+using (var activity = myActivitySource.StartActivity("RootActivity"))
 {
     activity?.SetTag("foo", 1);
     activity?.SetTag("bar", "Hello, World!");
     activity?.SetTag("baz", new[] { 1, 2, 3 });
     activity?.SetStatus(ActivityStatusCode.Ok);
+
+    using (var client = new HttpClient())
+    {
+        using (var slow = myActivitySource.StartActivity("SomethingSlow"))
+        {
+            await client.GetStringAsync("https://httpstat.us/200?sleep=1000");
+            await client.GetStringAsync("https://httpstat.us/200?sleep=500");
+        }
+
+        using (var fast = myActivitySource.StartActivity("SomethingFast"))
+        {
+            await client.GetStringAsync("https://httpstat.us/301");
+        }
+    }
 }
 
 Console.WriteLine("Press any key...");
